830390faf7f385bca767128e5dbff851393a9c75
Kenneth Mburu <kenneth.mburu@student.moringaschool.com>
2024-04-15 07:24:56 +0300

    logo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import './Profile.css'
+import Logo from './Assets/LOGO.png'
 import { auth } from './firebase'; // Assuming you have initialized Firebase Authentication
 
 export default function Profile() {
@@ -27,7 +28,7 @@ export default function Profile() {
                 <div className="profile-container">
                     <div className="navbar">
                         <div className="logo">
-                            <img src="logo.png" alt="Logo" />
+                            <img src={Logo} alt="Logo" />
                         </div>
                         <ul>
                             <li><a href='#home'>Home</a></li>
